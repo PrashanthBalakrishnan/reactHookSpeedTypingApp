f06f7bce798a1f0a582cbabebb8b441e1d23ca21
Prashanth Balakrishnan <prashbala13@gmail.com>
2022-10-12 00:29:12 -0400

    added all hooks to folder and created custom hook

--- typing-game/src/App.js
+++ typing-game/src/App.js
@@ -1,52 +1,23 @@
-import React,{useEffect, useState} from "react";
+import React from "react";
+import useWordGame from "./hooks/useWordGame";
 function App() {
-  const STARTING_TIME = 5
-  const[text,setText] = useState("")
-  const[timeRemaining,setTimeRemaining] = useState(STARTING_TIME)
-  const[isTimeRunning,setIsTimeRunning] = useState(false)
-  const [wordCount, setWordCount] = useState(0)
-
-  const handleChange = (e)=>{
-    const {value} = e.target
-      setText(value)
-  }
-  
-  const calculateWordCount=(text)=>{
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  }
-    
- 
-  useEffect(()=>{
-    if(timeRemaining&&isTimeRunning>0){
-      setTimeout(()=>{
-        setTimeRemaining(time => time - 1)
-      },1000)}
-      else if(timeRemaining===0){
-       endGame()
-      }
-    },[timeRemaining,isTimeRunning])
-       
-      
-        
-    const startGame =  () =>{
-      setIsTimeRunning(true)
-      setTimeRemaining(STARTING_TIME) 
-      setText(" ")
-    }
-    
-    const endGame = () =>{
-      setIsTimeRunning(false)
-      setWordCount(calculateWordCount(text))
-    } 
-
+  const {
+    typeRef, 
+    handleChange, 
+    text, 
+    isTimeRunning, 
+    timeRemaining, 
+    startGame, 
+    wordCount
+} = useWordGame()
   return (
     <div className="App">
         <h1>How fast do you type?</h1>
         <textarea 
+          ref={typeRef}
           onChange={handleChange}
           value={text}
-          disabled={!timeRemaining}/>
+          disabled={!isTimeRunning}/>
         <h4>Time remaining:{timeRemaining}</h4>
         <button disabled={isTimeRunning} onClick={startGame}>Start</button>
         <h1>Word count:{wordCount}</h1>
